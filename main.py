44c529b3f167ba994ea53744ba4e0e7403e6f708
fabian judohandoko <fabian240503@gmail.com>
2024-10-14 16:20:01 +0800

    tidy up

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-### prompt question
-### using lc_agents to call the tool in call_model
-'''
-main.py (question) ---> 
-call_model.py (question) ---> array of inputs
-lc_agents.py (array of inputs) ---> output
-'''
-
 from dotenv import load_dotenv
 import numpy
 import os
@@ -15,18 +7,27 @@ load_dotenv
 
 from call_model import call_model
 
-question = "I want to design a catalyst using 5 grams of gold with a CI of 5g, at temperature of 450 with TOS of 4"
-# numpy array of dimensions of the model
-elements_array = call_model(question)
-assert type(elements_array) == numpy.ndarray, "Not NumPy array"
 
-# Model Call
-model = pickle.load(open("RF_CO.pkl", "rb"))
+def answer_question(question: str) -> int:
+    """
+    Return the answer to the question
+    
+    Args:
+        - question(str)
+    Returns:
+        - int CO conversion of said experiment
+    """
+    # numpy array of dimensions of the model
+    elements_array = call_model(question)
+    assert type(elements_array) == numpy.ndarray, "Not NumPy array"
 
-def predict(elements):
-    prediction = model.predict([elements])
+    # Model Call
+    model = pickle.load(open("RF_CO.pkl", "rb"))
+
+    prediction = model.predict([elements_array])
 
     return prediction
 
-print(predict(elements_array))
+question = "I want to design a catalyst using 5 grams of gold with a CI of 5g, at temperature of 450 with TOS of 4"
+print(answer_question(question))
 
