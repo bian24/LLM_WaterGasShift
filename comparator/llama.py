843ed1a1abbff1d4a9d1809d42c669d3c5832064
fabian <fabian240503@gmail.com>
2025-03-03 16:58:18 +0800

    progress 3 mar

--- comparator/llama.py
+++ comparator/llama.py
@@ -1,27 +1,30 @@
 import requests
 import json
 
-# Define the URL and the payload
-url = 'http://localhost:11434/api/generate'
-payload = {
-    "model": "llama3",
-    "prompt": "Why is the sky blue?"
-}
+class llama:
+    def __init__(self):
+        self.url = 'http://localhost:11434/api/generate'
+        self.payload = None
+    
+    def generate_answer_llama(self, query):
+        """
+        Generate answer using local llama
+        """
+        self.payload = {
+            "model": "llama3.2",
+            "prompt": query
+        }
+        data = json.dumps(self.payload)
 
-# Convert the payload to a JSON string
-data = json.dumps(payload)
-
-# Make the POST request
-response = requests.post(url, data=data, headers={'Content-Type': 'application/json'})
-
-print(response)
-list_dict_words = []
-for each_word in response.text.split("\n"):
-    try:
-        data = json.loads(each_word) 
-    except:
-        pass
-    list_dict_words.append(data)
+        response = requests.post(self.url, data=data, headers={'Content-Type': 'application/json'})
+        list_dict_words = []
+        for each_word in response.text.split("\n"):
+            try:
+                data = json.loads(each_word) 
+            except:
+                pass
+            list_dict_words.append(data)
+                
+        llama_response = " ".join([word['response'] for word in list_dict_words if type(word) == type({})])
         
-llama_response = " ".join([word['response'] for word in list_dict_words if type(word) == type({})])
-print(llama_response)
+        return llama_response
