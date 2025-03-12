3cef1a8868c043e9f62a5174caafc55849f04e2e
fabian <fabian240503@gmail.com>
2025-03-12 16:21:02 +0800

    different comparator

--- comparator/comparator.py
+++ comparator/comparator.py
@@ -4,7 +4,7 @@ from langchain_openai import ChatOpenAI
 from ragas import EvaluationDataset
 from ragas import evaluate
 from ragas.llms import LangchainLLMWrapper
-from ragas.metrics import RougeScore, StringPresence, FactualCorrectness
+from ragas.metrics import ResponseRelevancy, FactualCorrectness, Faithfulness
 
 from main.rag import RAG
 
@@ -22,8 +22,9 @@ FOLDER_PATH = "script"
 RAG_VER= "rag_2"
 FILE = f"{FOLDER_PATH}/{RAG_VER}"
 
+rag = RAG()
 
-# Question and Answer CSV
+# Question and Ground Truth CSV
 question_csv = open(f"{FILE}.csv", mode="r", encoding="utf-8")
 question_read = csv.reader(question_csv)
 ground_truth_csv = open(f"{FILE}_ground_truth.csv", mode="r", encoding="utf-8")
@@ -31,15 +32,14 @@ ground_truth_read = csv.reader(ground_truth_csv)
 answer_csv = open(f"{FILE}_answer.csv", mode="r", encoding="utf-8")
 answer_read = csv.reader(answer_csv)
 
+
 dataset = []
-# Initialize
-rag = RAG()
 
 for query, ground_truth, answer in zip(question_read, ground_truth_read, answer_read):
     dataset.append(
         {
             "user_input": query[0],
-            "retrieved_contexts": rag.get_most_relevant_content(str(query)),
+            "retrieved_contexts": [(''.join(rag.get_most_relevant_content(str(query[0]))))],
             "response": answer[0],
             "reference": ground_truth[0]
         }
@@ -50,8 +50,7 @@ evaluation_dataset = EvaluationDataset.from_list(dataset)
 evaluator_llm = LangchainLLMWrapper(ChatOpenAI(model=MODEL))
 result = evaluate(
     dataset=evaluation_dataset, 
-    metrics=[RougeScore(), StringPresence(), FactualCorrectness()], 
+    metrics=[FactualCorrectness(), Faithfulness(), ResponseRelevancy()], 
     llm=evaluator_llm
 )
-
-print(result)
+result.upload()
